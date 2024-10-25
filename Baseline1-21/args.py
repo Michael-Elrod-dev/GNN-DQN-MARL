972f280e3013f604b7e0200b9af6f832048ee82e
Michael-Elrod-dev <michaelelrod.dev@gmail.com>
2024-10-25 11:17:30 -0400

    .

--- Baseline1-21/args.py
+++ Baseline1-21/args.py
@@ -6,10 +6,10 @@ class Args:
     def __init__(self):
         # Env Parameters
         self.env              = None
-        self.num_agents       = 30
-        self.num_goals        = 150
+        self.num_agents       = 4
+        self.num_goals        = 20
         self.num_obstacles    = 0
-        self.grid_size        = 61
+        self.grid_size        = 21
         self.agent_view_size  = 9
         self.action_size      = 4
         self.obs_size         = ((1 + self.num_goals) * 2) + self.num_goals
@@ -23,10 +23,10 @@ class Args:
 
         # Training parameters
         self.total_steps      = 2500000
-        self.episode_steps    = 300
+        self.episode_steps    = 150
         self.eps_start        = 1.0
         self.eps_end          = 0.01
-        self.eps_percentage   = 0.80
+        self.eps_percentage   = 0.70
         self.seed             = int(time.time())
 
         # Network Parameters
@@ -62,13 +62,13 @@ class Args:
         self.device       = 'cuda'
         self.load_policy  = False    # Evaluate a learned policy
         self.logger       = False     # Log training data to Wandb
-        self.render       = False     # Render the environment
+        self.render       = True     # Render the environment
         self.debug        = False     # Allow manual action inputs
 
         # Reward Parameters
         self.reward_goal = 5
-        self.penalty_goal = 0
-        self.penalty_obstacle = 0
+        self.penalty_goal = -1
+        self.penalty_obstacle = -5
         self.penalty_invalid_move = -5
 
         # Derived Parameters
